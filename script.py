b8e1e5b5052603c30ecd115fc01d8f6887ce4bb2
Britany Toapanta <btoapantah@est.ups.edu.ec>
2026-05-04 15:26:13 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,30 @@
+#script de python para probar git
+#autor:Britany Toapanta btoapantah@est.ups.edu
+#fecha:04/05/2026
+
+def main():
+    nombre = input("Como te llamas: ")
+    print ("Hola",nombre, "bienvenido a mi mundo")
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
