aa0fdfc699b1634e616f2cdf24b3b18260fd4421
Britany Toapanta <btoapantah@est.ups.edu.ec>
2026-05-04 15:39:32 -0500

    Cambiar saludo

--- script.py
+++ script.py
@@ -3,7 +3,7 @@
 #fecha:04/05/2026
 
 def main():
-    nombre = input("Como te llamas: ")
+    nombre = input("Buenis dias, Como te llamas: ")
     print ("Hola",nombre, "bienvenido a mi mundo")
 if __name__ == "__main__":
     main()
